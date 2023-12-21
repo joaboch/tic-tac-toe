21cb0191031ac63b6e8768929fe0d8fdf3fbcf4b
joaboch <joakoboch@gmail.com>
2023-12-21 20:58:47 -0300

    Modify code

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 //Store the gameboard as an array inside of a Gameboard object
 
-function createGameboard () {
-  const gameboardArr = 
-  ['', '', ''
-  , '', '', ''
-  , '', '', ''];
-  return {gameboardArr};
-}
+const gameboard = (function () {
+  const gameboardArr =
+    ['', '', '', '', '', '', '', '', ''];
+  return { gameboardArr };
+})();
 
-const gameboard = createGameboard();
 
-function playerMove (move) {
+function player(move) {
   opt = () => prompt('1 - 9') - 1;
-  return {move, opt};
+  return { move, opt };
 }
 
-const player1 = playerMove('X');
-const player2 = playerMove('O');
-
-function controlGame () {
-
-}
-
+const player1 = player('X');
+const player2 = player('O');
